5c31a6e1a260eeb4888d0450f9c0a585c1e9d612
AbudDaas <abdulrazakdaas0@gmail.com>
2025-01-25 11:39:15 +0300

    fix: use wepbpack bundler

---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer') 
+const webpack = require("webpack");
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "assets/[name].[ext]",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      /* Images Loader */
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: "asset/resource",
+      },
+      /* Style Sheet Loader*/
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      /* NodeJS Loader*/
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      /* File Loader */
+      {
+        test: /\.glb$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets/resource/",
+            },
+          },
+        ],
+      },
+      // Vue Loader
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: "vue-loader",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack-Template-App",
+      favicon: "./src/assets/favicon.ico",
+      filename: "index.html",
+      template: "public/template.html",
+    }),
+    new BundleAnalyzerPlugin(),
+    // add vue-loader plugin
+    new VueLoaderPlugin(),
+    /**
+     * to remove warn in browser console: runtime-core.esm-bundler.js:3607
+     * Feature flags __VUE_OPTIONS_API__, __VUE_PROD_DEVTOOLS__ are not explicitly defined..
+     */
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: true,
+    }),
+  ],
+};
